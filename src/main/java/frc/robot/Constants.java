b17238ffc8cced1dda894700cfa4ea45c4739abe
m00nb0ygame8 <52793898+m00nb0ygame8@users.noreply.github.com>
2026-03-13 10:50:37 -0400

    Added correct motor ids to the Intake Angle and Flywheel motor.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -61,8 +61,8 @@ public final class Constants {
 
     /** Constants for the Intake subsystem */
     public final class Intake {
-        public static int INTAKE_ANGLE_MOTOR_ID = 0;
-        public static int INTAKE_FLYWHEEL_MOTOR_ID = 0;
+        public static int INTAKE_ANGLE_MOTOR_ID = 40;
+        public static int INTAKE_FLYWHEEL_MOTOR_ID = 55;
 
         public static class PID {
             public static int kP = 0;
