a13e740c32deef89ae34e8b945b4aa200633a49d
JNedd26 <jnedd2026@berkeleycarroll.org>
2026-03-16 14:51:58 -0400

    Added Xbox controller support for intake
    Commented out drivetrain initialization and default command.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,29 +1,33 @@
 package frc.robot;
-
+import frc.robot.subsystems.Intake;
 import frc.robot.Constants.Controllers;
-import frc.robot.commands.subsystem.DefaultDrivetrainCommand;
+// import frc.robot.commands.subsystem.DefaultDrivetrainCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.drive.Drivetrain;
+// import frc.robot.subsystems.drive.Drivetrain;
 
 public class RobotContainer {
 
     private final CommandXboxController driverController =
             new CommandXboxController(Controllers.DRIVER_PORT);
+    private final Intake intake = new Intake();
 
 
-    private final Drivetrain drivetrain = new Drivetrain();
+    // private final Drivetrain drivetrain = new Drivetrain();
     public RobotContainer() {
         configureBindings();
     }
 
     private void configureBindings() {
 
-        drivetrain.setDefaultCommand(
-                new DefaultDrivetrainCommand(
-                        drivetrain,
-                        this.driverController
-                )
-        );
+        // drivetrain.setDefaultCommand(
+        //         new DefaultDrivetrainCommand(
+        //                 drivetrain,
+        //                 this.driverController
+        //         )
+        // );
+        
+
+        driverController.b().toggleOnTrue(intake.startupFlywheel());
 
         //System.out.println(Arrays.toString(this.drivetrain.getEncoderValues()));
     }
