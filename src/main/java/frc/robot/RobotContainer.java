38cfb5b2ebf940e8e7663f5ab4986830fef923bb
drivetrain-laptop <bert@berkeleycarroll.org>
2026-04-07 14:53:44 -0400

    initial commands for getting the balls to fire

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,27 +3,34 @@ import frc.robot.Constants.IDs;
 import frc.robot.commands.subsystem.DefaultDrivetrainCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.drive.Drivetrain;
 
 public class RobotContainer {
 
-    private final CommandXboxController driverController =
-            new CommandXboxController(IDs.Controllers.Driver);
+    private final CommandXboxController driverController = new CommandXboxController(IDs.Controllers.Driver);
+    private final CommandXboxController operatorController = new CommandXboxController(IDs.Controllers.Operator);
     private final Drivetrain drivetrain = new Drivetrain();
-
+    private final Shooter shooter = new Shooter();
     public RobotContainer() {
+        configureDashboard();
         configureBindings();
     }
 
     private void configureBindings() {
 
         drivetrain.setDefaultCommand(
-                new DefaultDrivetrainCommand(
-                        drivetrain,
-                        this.driverController
-                )
+            new DefaultDrivetrainCommand(
+                drivetrain,
+                this.driverController
+            )
         );
+
+        operatorController.a().whileTrue(shooter.fireFuelCommand());
+    }
+
+    private void configureDashboard(){
         SmartDashboard.putData(drivetrain);
-        //System.out.println(Arrays.toString(this.drivetrain.getEncoderValues()));
+        SmartDashboard.putData(shooter);
     }
 }
