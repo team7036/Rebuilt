3ebcd4f69c9e4f3231c05fcda7fa6393a9725961
drivetrain-laptop <bert@berkeleycarroll.org>
2026-04-09 10:28:27 -0400

    start separating intake commands into separate files.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -9,18 +8,14 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.units.Unit;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.commands.intake.SetIntakeAngleCommand;
 
 /**
  * The Intake for the robot:
@@ -34,12 +29,9 @@ import frc.robot.Constants;
  */
 
 public class Intake extends SubsystemBase {
+
     private final SparkMax angleMotor, flywheelMotor;
     private final SparkMaxConfig angleConfig, flywheelConfig;
-    private final ProfiledPIDController anglePID;
-    private final ArmFeedforward angleFeedForward;
-
-    private double targetAngle = 0.0;
 
     /**
      * Constructs a new intake
@@ -49,21 +41,6 @@ public class Intake extends SubsystemBase {
         this.angleConfig = new SparkMaxConfig();
         this.flywheelMotor = new SparkMax(Constants.Intake.INTAKE_FLYWHEEL_MOTOR_ID, MotorType.kBrushless);
         this.flywheelConfig = new SparkMaxConfig();
-        this.anglePID = new ProfiledPIDController(
-            Constants.Intake.AnglePID.kP,
-            Constants.Intake.AnglePID.kI,
-            Constants.Intake.AnglePID.kD,
-            new TrapezoidProfile.Constraints(
-                Constants.Intake.ANGLE_MAX_VELOCITY, 
-                Constants.Intake.ANGLE_MAX_ACCELERATION
-            )
-        );
-
-        this.angleFeedForward = new ArmFeedforward(
-            Constants.Intake.AngleFeedForward.kS,
-            Constants.Intake.AngleFeedForward.kG,
-            Constants.Intake.AngleFeedForward.kV
-        );
         configureMotors();
         this.setDefaultCommand(stowIntakeCommand());
     }
@@ -92,11 +69,11 @@ public class Intake extends SubsystemBase {
         return angleMotor.getEncoder().getPosition();
     }
 
-    private void setAngle(double angle){
-        targetAngle = angle;
+    public void setAngleVoltage(double volts){
+        angleMotor.setVoltage(volts);
     }
 
-    private boolean isLowered(){
+    private boolean canIntake(){
         return getAngle() >= Constants.Intake.LOWERED_THRESHOLD;
     }
 
@@ -105,30 +82,20 @@ public class Intake extends SubsystemBase {
     }
 
     public Command lowerIntakeCommand(){
-        return this.run(()->setAngle(Constants.Intake.INTAKING_ANGLE));
+        return new SetIntakeAngleCommand(this, Constants.Intake.INTAKING_ANGLE);
     }
 
     public Command raiseIntakeCommand(){
-        return this.run(()->setAngle(Constants.Intake.STOWED_ANGLE));
+        return this.run(()->{});
     }
 
     public Command intakeFuelCommand(){
-        return this.run(()->flywheelMotor.set(Constants.Intake.FLYWHEEL_SPEED)).onlyWhile(this::isLowered);
-    }
-
-    @Override
-    public void periodic(){
-        double pid = anglePID.calculate(getAngle(), targetAngle);
-        double ff = angleFeedForward.calculate(targetAngle - Constants.Intake.OFFSET_ANGLE, 0);
-        angleMotor.setVoltage(pid + ff);
+        return this.run(()->flywheelMotor.set(Constants.Intake.FLYWHEEL_SPEED)).onlyIf(this::canIntake);
     }
 
     public void initSendable(SendableBuilder builder) {
         builder.setSmartDashboardType("ShooterSubsystem");
         builder.addDoubleProperty("angle/measured", this::getAngle, null);
-        builder.addDoubleProperty("angle/goal", ()->anglePID.getGoal().position, null);
-        builder.addDoubleProperty("angle/positionError", anglePID::getPositionError, null);
-        SmartDashboard.putData("Intake/AnglePID", anglePID);
         SmartDashboard.putData("Intake/raise", raiseIntakeCommand());
         SmartDashboard.putData("Intake/lower", lowerIntakeCommand());
     }
